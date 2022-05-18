a593a34fe62d12515d66f4eb49b2e8e2de11e3c4
AlmTeo <almmondducherr@gmail.com>
2022-05-18 20:28:36 +0300

    Retaining user login and redirecting correct pages

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager, login_manager, login_required
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
-
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'THIS IS A SECRET KEY qweasd'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     
     from .views import views
@@ -18,7 +19,16 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
     
     from .models import User, Note
+
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
     
     return app
 
--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -10,11 +11,11 @@ def login():
     if request.method == 'POST':
         email =  request.form.get('email')
         password =  request.form.get('password')
-        
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
@@ -24,8 +25,10 @@ def login():
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -50,6 +53,7 @@ def sign_up():
             new_user = User(email=email, nickname=nickname, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('Account created successfully!', category='success')
             return redirect(url_for('views.home'))
             
