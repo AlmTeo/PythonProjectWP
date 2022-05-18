8095f15d3d907da28b03b413f749f1e98ef047e9
AlmTeo <almmondducherr@gmail.com>
2022-05-18 21:59:27 +0300

    Saving and displaying user's data and notes

--- website/auth.py
+++ website/auth.py
@@ -22,7 +22,7 @@ def login():
         else:
             flash('Email does not exist.', category='error')    
         
-    return render_template("login.html", boolean=True)
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 @login_required
@@ -53,8 +53,8 @@ def sign_up():
             new_user = User(email=email, nickname=nickname, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash('Account created successfully!', category='success')
             return redirect(url_for('views.home'))
             
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
