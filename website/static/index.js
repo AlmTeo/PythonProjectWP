b359f6d53dd0f972a96995bb6aad3a21bc5a59a5
AlmTeo <almmondducherr@gmail.com>
2022-05-21 13:34:20 +0300

    JS functions for note updating

--- website/static/index.js
+++ website/static/index.js
@@ -6,3 +6,31 @@ function deleteNote(noteId) {
     window.location.href = "/";
   });
 }
+
+function updateNote(noteId) {
+  var noteContents = document.getElementById('edit-note').value
+  fetch("/update-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId , noteContents: noteContents}),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+}
+
+$(document).on('click', '.edit-btn', function() {
+  var noteId = $(this).parents('tr').find('.note-text').attr('data-note-id');
+  var noteText = $(this).parents('tr').find('.note-text').text();
+  $('.note-edit-input').val(noteText);
+  $('#modalContactForm').attr('data-edited-note', noteId);
+});
+
+$(document).on('click', '.save-edit-note', function() {
+  var noteId = $('#modalContactForm').attr('data-edited-note');
+  var noteText = $('.note-edit-input').val();
+  fetch("/update-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId , noteContents: noteText}),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+});
